58c72ab9b8e42ae43c7dd6b4e8b9d76287c956b4
umbaranowska <umbaranowska@gmail.com>
2023-05-04 22:44:40 +0200

    update dataset and gitignore

--- 01_data_scraping/data_scraping.py
+++ 01_data_scraping/data_scraping.py
@@ -2,5 +2,5 @@ from redditscraper import *
 
 RedditScraper = RedditScraper()
 # RedditScraper.run_all()
-RedditScraper.get_all_threads()
+# RedditScraper.get_all_threads()
 # RedditScraper.merge_threads_to_csv()
