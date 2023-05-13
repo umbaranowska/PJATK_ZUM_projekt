0966b6a1be6584eda539b64c482d5e58a0eb87c4
umbaranowska <umbaranowska@gmail.com>
2023-05-13 16:50:20 +0200

    added new data for threads started after last scraping

--- 01_data_scraping/data_scraping.py
+++ 01_data_scraping/data_scraping.py
@@ -3,4 +3,6 @@ from redditscraper import *
 RedditScraper = RedditScraper()
 # RedditScraper.run_all()
 # RedditScraper.get_all_threads()
-# RedditScraper.merge_threads_to_csv()
+RedditScraper.get_single_thread(url = "https://www.reddit.com/r/taskmaster/comments/137osj0/taskmaster_s15e06_its_my_milk_now_discussion/")
+RedditScraper.get_single_thread(url = "https://www.reddit.com/r/taskmaster/comments/13epy1e/taskmaster_s15e07_schrodingers_egg_discussion/")
+RedditScraper.merge_threads_to_csv()
